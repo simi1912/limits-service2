575f879a02c03e77c163d5154657d75342ff7235
simi1912 <nioasca@gmail.com>
2021-04-16 15:34:10 +0300

    Initial commit

---
+++ src/main/java/com/simi/microservices2/limitsservice2/LimitsService2Application.java
@@ -0,0 +1,13 @@
+package com.simi.microservices2.limitsservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsService2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitsService2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/simi/microservices2/limitsservice2/LimitsService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simi.microservices2.limitsservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsService2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
